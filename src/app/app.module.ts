26859c2719e28ebd027ff76aa8b0572400c04d55
Mayur-Baraiya <mayurbaraiya12242@gmail.com>
2018-07-24 12:24:50 -0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
+import {routing} from './app.routing';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    NavbarComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
